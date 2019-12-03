6fb05f7d5a5a4ca49d8d75a9ef6796a0b8ec8823
shohirev <vishohirev@yandex.ru>
2019-12-03 19:25:55 +0500

    refactoring parser

--- src/parsers.js
+++ src/parsers.js
@@ -3,23 +3,18 @@ import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const getFileType = file => path.extname(`${file}`);
-
 const parser = (file) => {
-	const extension = getFileType(file);
-	let parsedFile;
-	switch (extension) {
+	switch (path.extname(`${file}`)) {
 		case '.json':
-		parsedFile = JSON.parse(fs.readFileSync(`${file}`, 'utf-8'));
+		return JSON.parse(fs.readFileSync(`${file}`, 'utf-8'));
 		break;
 		case '.yml':
-		parsedFile = yaml.safeLoad(fs.readFileSync(`${file}`, 'utf-8'));
+		return yaml.safeLoad(fs.readFileSync(`${file}`, 'utf-8'));
 		break;
 		case '.ini':
-		parsedFile = ini.parse(fs.readFileSync(`${file}`, 'utf-8'));
+		return ini.parse(fs.readFileSync(`${file}`, 'utf-8'));
 		break;
 	}
-	return parsedFile;
 };
 
 export default parser;
